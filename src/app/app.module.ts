db19dcf939a03d629428f0fb1e6fe9bfbf7742bc
Astre-bit <astre.series@gmail.com>
2020-11-08 22:11:30 +0100

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
-import { AngularFontAwesomeModule } from "angular-font-awesome";
 
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { BodyComponent } from "./body/body.component";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AngularFontAwesomeModule],
+  imports: [BrowserModule, FormsModule],
   declarations: [AppComponent, HeaderComponent, BodyComponent, FooterComponent],
   bootstrap: [AppComponent]
 })
